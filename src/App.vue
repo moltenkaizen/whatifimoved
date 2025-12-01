52198d387d3506499cb14a9d3cf92e64a5e8c6c8
Steve <steve@Steves-MacBook-Pro.local>
2025-11-30 18:55:57 -0800

    Add Vercel Analytics and Speed Insights tracking

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <AddressCompare />
+  <Analytics />
+  <SpeedInsights />
 </template>
 
 <script setup>
 import AddressCompare from './components/AddressCompare.vue'
+import { Analytics } from '@vercel/analytics/vue'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 </script>
 
 <style scoped>
